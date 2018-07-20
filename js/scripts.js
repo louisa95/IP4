1fdd10521fc711898a19569a53dd438938efc5be
louisa95 <louisakonchellah@gmail.com>
2018-07-20 08:20:35 +0300

    initialized git

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+//Initialize human score to 0
+//Initialize computer score to 0
+//WHILE computer score < 100 AND human score < 100
+ 
+    //DO
+        //Calculate human roll
+        //IF human roll == 1 THEN
+            //DISPLAY your turn is over
+        //ELSE
+            //DISPLAY Roll again or hold?
+            //INPUT choice            
+            //IF choice is hold THEN
+                //Add human roll to human score
+            //ENDIF
+        //ENDIF
+    //WHILE human roll <> 1 AND choice <> hold
+ 
+ 
+    //Initialize subtotal to 0
+    //DO
+        C//alculate computer roll
+        //IF computer roll <> 1 THEN
+        //Add computer roll to computerscore
+            //Add computer roll to subtotal
+        //ENDIF
+    //WHILE computer roll <> 1 AND subtotal <= 20)
+ 
+//ENDWHILE
+ 
+//DISPLAY WINNER
